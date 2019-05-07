24fbc8da7e957e22d930576b64f43f035ce6ff65
Chaitanya K <kamthamc@users.noreply.github.com>
2019-05-07 09:55:04 -0700

    feat: updated markdown styles (#18)

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,11 @@ import { createGlobalStyle } from "styled-components";
 
 
 const GlobalStyle = createGlobalStyle`
+
+  @import 'https://fonts.googleapis.com/css?family=Fira+Sans:300,300i,400,400i,500,500i,700,700i&subset=latin-ext';
+  @import 'https://fonts.googleapis.com/css?family=Roboto+Mono:300,400&subset=latin-ext';
+  @import 'https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css';
+
   html, body, #app {
     height: 100%;
     margin: 0;
