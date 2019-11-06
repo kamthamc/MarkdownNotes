5fdb324b625d04231813228d1061a6fb859f9a0f
Chaitanya Kumar Kamatham <kamthamc@users.noreply.github.com>
2019-11-06 12:56:01 -0800

    fix(build): fixes build

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,78 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
-	entry: './src/index.tsx',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[contenthash].js'
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		port: process.env.PORT || 8080
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.(ts|tsx)?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.(woff(2)?|ttf|eot|svg)$/,
-				use: 'file-loader'
-			},
-			{
-				test: /\.css$/,
-				use: 'css-loader'
-			}
-		]
-	},
-	resolve: {
-		extensions: [
-			'.js',
-			'.jsx',
-			'.tsx',
-			'.ts'
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: require('html-webpack-template'),
-			inject: false,
-			appMountId: 'app',
-			lang:'en-us',
-			title: 'Markdown Notes',
-			favicon: path.resolve(__dirname, 'resources', 'iconfinder_pages_1118205.png'),
-			meta: [
-				{
-					name: 'viewport',
-					width: 'device-width',
-					'initial-scale': 1,
-					'user-scalable': 'no',
-				}
-			]
-		})
-	],
-	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\\/]node_modules[\\\/]/,
-					name: 'vendors',
-					chunks: 'all'
-				}
-			}
-		}
-	}
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: process.env.PORT || 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: require('html-webpack-template'),
+      inject: false,
+      appMountId: 'app',
+      lang: 'en-us',
+      title: 'Markdown Notes',
+      favicon: path.resolve(
+        __dirname,
+        'resources',
+        'iconfinder_pages_1118205.png'
+      ),
+      meta: [
+        {
+          name: 'viewport',
+          width: 'device-width',
+          'initial-scale': 1,
+          'user-scalable': 'no'
+        }
+      ]
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\\/]node_modules[\\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
 };
 
 module.exports = config;
